af84e5f841831f1308dfea6934d307cdf444afb2
MichellCazares <mc.michellcazares@gmail.com>
2023-07-21 18:01:25 -0600

    Se agregó la página principal, sus estilos y su script

---
+++ js/main.js
@@ -0,0 +1,18 @@
+// Auto Type
+const typed = new Typed('.homeMultipleText', {
+    strings: ['Desarrollador Java FullStack JR', 'Ing. En Sistemas Computacionales'],
+    typeSpeed: 50,
+    backSpeed: 50,
+    backDelay: 1000,
+    loop: true
+});
+
+
+// Auto Type
+const typed2 = new Typed('.aboutMeMultipleText', {
+    strings: ['Desarrollador Java FullStack JR', 'Ing. En Sistemas Computacionales'],
+    typeSpeed: 50,
+    backSpeed: 50,
+    backDelay: 1000,
+    loop: true
+});
